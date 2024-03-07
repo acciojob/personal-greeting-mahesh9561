409d537cefd4bc4b0212cf778d1997bed1556b66
Mahesh Pathak <maheshpathak200@gmail.com>
2024-03-07 19:27:04 +0530

    cds

--- src/components/App.js
+++ src/components/App.js
@@ -14,6 +14,7 @@ const App = () => {
     <div>
       {/* Do not remove the main div */}
       {/* <p>Enter your name: </p> */}
+      <label htmlFor=""> Enter your name: </label>
       <input
         type="text"
         placeholder="Enter your name"
