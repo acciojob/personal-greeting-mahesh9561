f982bf5158259caac3fb254565bcbe2b1efeacab
Mahesh Pathak <maheshpathak200@gmail.com>
2024-03-07 19:24:16 +0530

    input

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,27 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [name, setName] = useState('');
+
+  // Event handler for input change
+  const handleInputChange = (event) => {
+    setName(event.target.value);
+  };
   return (
+
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <p>Enter your name: </p>
+      <input
+        type="text"
+        placeholder="Enter your name"
+        value={name}
+        onChange={handleInputChange}
+      />
+      {/* Display personalized greeting */}
+      {name && <p>Hello, {name}!</p>}
     </div>
   )
 }
